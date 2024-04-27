00ebb35993707b3e1eac05d44b284820a56ebb43
imaimai17468 <tosiki_801_life@outlook.jp>
2024-04-28 00:09:50 +0900

    feat: ヘッダーコンポーネントにロゴを追加

--- src/components/parts/Header/Header.tsx
+++ src/components/parts/Header/Header.tsx
@@ -6,24 +6,17 @@ import {
   Box,
   Card,
   Flex,
-  Text,
   UnstyledButton,
 } from "@mantine/core";
 import { BellIcon } from "@radix-ui/react-icons";
+import { Logo } from "../Logo";
 
 export const Header: React.FC = () => {
   return (
     <Box py={16} pl={32} pr={16} style={{ position: "sticky", top: 0 }}>
       <Flex justify="space-between" align="center">
         <Anchor href={PATHS.TOP}>
-          <Text
-            size="xl"
-            fw={700}
-            variant="gradient"
-            gradient={{ from: "blue", to: "cyan", deg: 90 }}
-          >
-            Tech Tips
-          </Text>
+          <Logo />
         </Anchor>
         <Card shadow="md" radius="md" py={8} px={24}>
           <Flex gap={16} align="center">
