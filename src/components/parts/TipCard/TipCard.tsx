ae46bda3f13d99520972ddc05ce516a722744c47
imaimai17468 <tosiki_801_life@outlook.jp>
2024-04-27 23:34:37 +0900

    feat: TipCardコンポーネントを追加

---
+++ src/components/parts/TipCard/TipCard.tsx
@@ -0,0 +1,30 @@
+import type { Tip } from "@/repositories/tips/types";
+import { Avatar, Card, Flex, Stack, Text } from "@mantine/core";
+import { HeartIcon } from "@radix-ui/react-icons";
+
+export const TipCard: React.FC<{ tip: Tip }> = ({ tip }) => {
+  return (
+    <Card shadow="xs" radius="md" padding="md">
+      <Stack gap={16} justify="space-between" h="100%">
+        <Stack gap={4}>
+          <Text lineClamp={2} size="lg" fw={700}>
+            {tip.title}
+          </Text>
+          <Text lineClamp={3}>{tip.description}</Text>
+        </Stack>
+        <Flex justify="space-between" align="end">
+          <Flex align="center" gap={8}>
+            <Avatar src={tip.author.userImageURL} alt={tip.author.username} />
+            <Text lineClamp={1}>{tip.author.username}</Text>
+          </Flex>
+          <Flex align="center" gap={8}>
+            <HeartIcon />
+            <Text size="xs" style={{ fontFamily: "sans-serif" }}>
+              {tip.favCount}
+            </Text>
+          </Flex>
+        </Flex>
+      </Stack>
+    </Card>
+  );
+};
