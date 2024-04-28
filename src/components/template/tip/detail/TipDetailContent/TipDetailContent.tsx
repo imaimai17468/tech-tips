5b0afc0791be6317ea3db7039f2cecfb8f8f2846
imaimai17468 <tosiki_801_life@outlook.jp>
2024-04-28 17:38:34 +0900

    TipDetailContentコンポーネントに新しいアクションアイコンを追加

--- src/components/template/tip/detail/TipDetailContent/TipDetailContent.tsx
+++ src/components/template/tip/detail/TipDetailContent/TipDetailContent.tsx
@@ -1,23 +1,55 @@
 import { createMockTip } from "@/repositories/tips/mock";
-import { Badge, Card, Flex, Stack, Text, Title } from "@mantine/core";
+import { ActionIcon, Anchor, Avatar, Badge, Card, Flex, Image, Stack, Text, Title } from "@mantine/core";
+import { BookmarkIcon, Link1Icon } from "@radix-ui/react-icons";
 
 export const TipDetailContent: React.FC = () => {
   const tip = createMockTip();
 
   return (
-    <Card shadow="xs" padding="xl" radius="lg">
-      <Stack gap={16}>
-        <Stack gap={4}>
-          <Title>{tip.title}</Title>
-          <Text size="xl">{tip.description}</Text>
+    <Flex gap={32}>
+      <Card shadow="xs" padding="xl" radius="lg">
+        <Stack gap={16}>
+          <Stack gap={4}>
+            <Title>{tip.title}</Title>
+            <Text size="xl">{tip.description}</Text>
+          </Stack>
+          <Flex gap={8}>
+            {tip.tags.map((tag) => (
+              <Badge key={tag}>{tag}</Badge>
+            ))}
+          </Flex>
+          <Text>{tip.content}</Text>
         </Stack>
+      </Card>
+      <Stack>
         <Flex gap={8}>
-          {tip.tags.map((tag) => (
-            <Badge key={tag}>{tag}</Badge>
-          ))}
+          <ActionIcon radius="xl" variant="outline" color="pink">
+            <BookmarkIcon />
+          </ActionIcon>
+          <ActionIcon radius="xl" variant="outline">
+            <Link1Icon />
+          </ActionIcon>
         </Flex>
-        <Text>{tip.content}</Text>
+        <Card shadow="xs" padding="xl" radius="lg" w={300} style={{ flexShrink: 0 }}>
+          <Stack gap={16}>
+            <Flex align="center" gap={8}>
+              <Avatar src={tip.author.userImageURL} alt={tip.author.username} />
+              <Anchor lineClamp={1} c="black" fw={700}>
+                {tip.author.username}
+              </Anchor>
+            </Flex>
+            <Text>{tip.author.bio}</Text>
+            <Flex gap={8}>
+              <ActionIcon radius="xl" variant="outline" color="black">
+                <Image src="/image/github-mark.svg" w={16} h={16} />
+              </ActionIcon>
+              <ActionIcon radius="xl" color="black">
+                <Image src="/image/logo.svg" w={16} h={16} />
+              </ActionIcon>
+            </Flex>
+          </Stack>
+        </Card>
       </Stack>
-    </Card>
+    </Flex>
   );
 };
