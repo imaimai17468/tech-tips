b598285578a5373b13bcb36a4b5af1bd2903ee2a
imaimai17468 <tosiki_801_life@outlook.jp>
2024-04-28 17:38:08 +0900

    ユーザーのモックデータのbioを更新

--- src/repositories/user/mock.ts
+++ src/repositories/user/mock.ts
@@ -5,7 +5,7 @@ export const createMockUser = (): User => {
   return {
     id: faker.string.nanoid(),
     username: faker.internet.userName(),
-    bio: faker.lorem.sentence(),
+    bio: faker.lorem.paragraph(),
     twitterUsername: faker.internet.userName(),
     githubUsername: faker.internet.userName(),
     userImageURL: faker.image.avatar(),
