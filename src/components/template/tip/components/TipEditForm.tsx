5405f062f8559fee2dfa8e2b5833a85f03421c20
imaimai17468 <tosiki_801_life@outlook.jp>
2024-06-20 13:28:09 +0900

    feat: form部分をコンポーネント分割

---
+++ src/components/template/tip/components/TipEditForm.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Editor } from "@/components/parts/Editor";
+import { type Tip, type TipForm, TipFormValidator } from "@/repositories/tips/types";
+import type { User } from "@/repositories/user/types";
+import { Box, Button, Card, Flex, Stack, Switch, TagsInput, Text, TextInput, Textarea } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { PaperPlaneIcon } from "@radix-ui/react-icons";
+import { zodResolver } from "mantine-form-zod-resolver";
+import { typeid } from "typeid-js";
+
+type Props = {
+  user: User;
+};
+
+export const TipEditForm: React.FC<Props> = ({ user }) => {
+  const form = useForm<TipForm>({
+    mode: "uncontrolled",
+    initialValues: {
+      title: "",
+      isPublic: false,
+      description: undefined,
+      content: undefined,
+      tags: undefined,
+    },
+    validate: zodResolver(TipFormValidator),
+  });
+
+  const handleFormSubmit = (values: TipForm) => {
+    const submitData: Tip = {
+      ...values,
+      id: typeid().toString(),
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+      author: user,
+    };
+
+    console.log(submitData);
+  };
+
+  return (
+    <Box w="100%">
+      <form onSubmit={form.onSubmit(handleFormSubmit)}>
+        <Stack gap={32}>
+          <Stack>
+            <Flex justify="end" align="center" gap={16}>
+              <Switch label="公開" key={form.key("isPublic")} {...form.getInputProps("isPublic")} />
+              <Button w="fit-content" type="submit">
+                <Flex align="center" gap={4}>
+                  <Text>Post</Text>
+                  <PaperPlaneIcon />
+                </Flex>
+              </Button>
+            </Flex>
+            <Card shadow="xs" padding="xl" radius="lg" h="fit-content" w="100%">
+              <Stack gap={16}>
+                <Stack gap={4}>
+                  <TextInput label="Title" key={form.key("title")} {...form.getInputProps("title")} />
+                  <Textarea
+                    label="Description"
+                    rows={3}
+                    key={form.key("description")}
+                    {...form.getInputProps("description")}
+                  />
+                </Stack>
+                <TagsInput
+                  label="Tags"
+                  description="Press Enter to submit a tag"
+                  key={form.key("tags")}
+                  {...form.getInputProps("tags")}
+                />
+              </Stack>
+            </Card>
+          </Stack>
+          <Card shadow="xs" padding="xl" radius="lg" h="fit-content" w="100%" mih="50vh">
+            <Editor key={form.key("contents")} {...form.getInputProps("contents")} />
+          </Card>
+        </Stack>
+      </form>
+    </Box>
+  );
+};
--- src/components/template/tip/create/TipCreateContent/TipCreateContent.tsx
+++ src/components/template/tip/create/TipCreateContent/TipCreateContent.tsx
@@ -1,113 +1,32 @@
-"use client";
-
-import { Editor } from "@/components/parts/Editor";
-import { type Tip, type TipForm, TipFormValidator } from "@/repositories/tips/types";
 import { createMockUser } from "@/repositories/user/mock";
-import {
-  ActionIcon,
-  Anchor,
-  Avatar,
-  Button,
-  Card,
-  Flex,
-  Image,
-  Stack,
-  Switch,
-  TagsInput,
-  Text,
-  TextInput,
-  Textarea,
-} from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { PaperPlaneIcon } from "@radix-ui/react-icons";
-import { zodResolver } from "mantine-form-zod-resolver";
-import { typeid } from "typeid-js";
+import { ActionIcon, Anchor, Avatar, Card, Flex, Image, Stack, Text } from "@mantine/core";
+import { TipEditForm } from "../../components/TipEditForm";
 
 export const TipCreateContent: React.FC = () => {
   const user = createMockUser();
 
-  const form = useForm<TipForm>({
-    mode: "uncontrolled",
-    initialValues: {
-      title: "",
-      isPublic: false,
-      description: undefined,
-      content: undefined,
-      tags: undefined,
-    },
-    validate: zodResolver(TipFormValidator),
-  });
-
-  const handleFormSubmit = (values: TipForm) => {
-    const submitData: Tip = {
-      ...values,
-      id: typeid().toString(),
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-      author: user,
-    };
-
-    console.log(submitData);
-  };
-
   return (
-    <form onSubmit={form.onSubmit(handleFormSubmit)}>
-      <Flex gap={32}>
-        <Stack w="100%" gap={32}>
-          <Stack>
-            <Flex justify="end" align="center" gap={16}>
-              <Switch label="公開" key={form.key("isPublic")} {...form.getInputProps("isPublic")} />
-              <Button w="fit-content" type="submit">
-                <Flex align="center" gap={4}>
-                  <Text>Post</Text>
-                  <PaperPlaneIcon />
-                </Flex>
-              </Button>
-            </Flex>
-            <Card shadow="xs" padding="xl" radius="lg" h="fit-content" w="100%">
-              <Stack gap={16}>
-                <Stack gap={4}>
-                  <TextInput label="Title" key={form.key("title")} {...form.getInputProps("title")} />
-                  <Textarea
-                    label="Description"
-                    rows={3}
-                    key={form.key("description")}
-                    {...form.getInputProps("description")}
-                  />
-                </Stack>
-                <TagsInput
-                  label="Tags"
-                  description="Press Enter to submit a tag"
-                  key={form.key("tags")}
-                  {...form.getInputProps("tags")}
-                />
-              </Stack>
-            </Card>
-          </Stack>
-          <Card shadow="xs" padding="xl" radius="lg" h="fit-content" w="100%" mih="50vh">
-            <Editor key={form.key("contents")} {...form.getInputProps("contents")} />
-          </Card>
+    <Flex gap={32}>
+      <TipEditForm user={user} />
+      <Card shadow="xs" padding="xl" radius="lg" w={300} style={{ flexShrink: 0 }} h="fit-content">
+        <Stack gap={16}>
+          <Flex align="center" gap={8}>
+            <Avatar src={user.userImageURL} alt={user.username} />
+            <Anchor lineClamp={1} c="black" fw={700}>
+              {user.username}
+            </Anchor>
+          </Flex>
+          <Text>{user.bio}</Text>
+          <Flex gap={8}>
+            <ActionIcon radius="xl" variant="outline" color="black">
+              <Image src="/image/github-mark.svg" w={16} h={16} />
+            </ActionIcon>
+            <ActionIcon radius="xl" color="black">
+              <Image src="/image/logo.svg" w={16} h={16} />
+            </ActionIcon>
+          </Flex>
         </Stack>
-        <Card shadow="xs" padding="xl" radius="lg" w={300} style={{ flexShrink: 0 }} h="fit-content">
-          <Stack gap={16}>
-            <Flex align="center" gap={8}>
-              <Avatar src={user.userImageURL} alt={user.username} />
-              <Anchor lineClamp={1} c="black" fw={700}>
-                {user.username}
-              </Anchor>
-            </Flex>
-            <Text>{user.bio}</Text>
-            <Flex gap={8}>
-              <ActionIcon radius="xl" variant="outline" color="black">
-                <Image src="/image/github-mark.svg" w={16} h={16} />
-              </ActionIcon>
-              <ActionIcon radius="xl" color="black">
-                <Image src="/image/logo.svg" w={16} h={16} />
-              </ActionIcon>
-            </Flex>
-          </Stack>
-        </Card>
-      </Flex>
-    </form>
+      </Card>
+    </Flex>
   );
 };
