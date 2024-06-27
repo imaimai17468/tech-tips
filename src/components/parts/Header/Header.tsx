e380957de8bbc8beb3bc8d7d811ab8cfd103305d
imaimai17468 <tosiki_801_life@outlook.jp>
2024-06-28 01:15:37 +0900

    fix: ヘッダーにプロファイルメニューを追加

--- src/components/parts/Header/Header.tsx
+++ src/components/parts/Header/Header.tsx
@@ -1,10 +1,14 @@
 import { CLIENT_PATHS } from "@/constants/clientPaths";
-import { Anchor, Avatar, Card, Flex, UnstyledButton } from "@mantine/core";
+import { Anchor, Card, Flex } from "@mantine/core";
 import { Logo } from "../Logo";
 import { RouteButton } from "./components/RouteButton";
 import { TipCreateButton } from "./components/TipCreateButton";
+import { UserButton } from "./components/UserButton";
+import { createMockUser } from "@/repositories/user/mock";
 
 export const Header: React.FC = () => {
+  const user = createMockUser();
+
   return (
     <Card shadow="md" radius="md" py={8} px={24} mx={16} mt={16}>
       <Flex justify="space-between" align="center">
@@ -15,9 +19,7 @@ export const Header: React.FC = () => {
           <RouteButton href={CLIENT_PATHS.TOP}>YourTips</RouteButton>
           <RouteButton href={CLIENT_PATHS.STACK}>Stack</RouteButton>
           <TipCreateButton />
-          <UnstyledButton>
-            <Avatar />
-          </UnstyledButton>
+          <UserButton href={user.userImageURL} userName={user.username} />
         </Flex>
       </Flex>
     </Card>
---
+++ src/components/parts/Header/components/UserButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Avatar, Menu, UnstyledButton } from "@mantine/core";
+import { ExitIcon, Pencil1Icon } from "@radix-ui/react-icons";
+
+type Props = {
+  href: string;
+  userName: string;
+};
+
+export const UserButton: React.FC<Props> = ({ href, userName }) => {
+  return (
+    <Menu offset={15} withArrow position="bottom-end" transitionProps={{ transition: "rotate-right", duration: 150 }}>
+      <Menu.Target>
+        <UnstyledButton>
+          <Avatar src={href} alt="profile-icon" />
+        </UnstyledButton>
+      </Menu.Target>
+      <Menu.Dropdown>
+        <Menu.Item>{userName}</Menu.Item>
+
+        <Menu.Label>Settings</Menu.Label>
+        <Menu.Item leftSection={<Pencil1Icon />}>Your Tips</Menu.Item>
+
+        <Menu.Label>Account</Menu.Label>
+        <Menu.Item leftSection={<ExitIcon />}>Logout</Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+  );
+};
