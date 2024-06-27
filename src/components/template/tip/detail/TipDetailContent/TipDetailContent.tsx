c380c7d80f22dbe6c7a8e2fa7cc154c63cbc8ee8
imaimai17468 <tosiki_801_life@outlook.jp>
2024-06-27 22:32:32 +0900

    fix: detailからeditボタンを削除

--- src/components/template/tip/detail/TipDetailContent/TipDetailContent.tsx
+++ src/components/template/tip/detail/TipDetailContent/TipDetailContent.tsx
@@ -2,13 +2,12 @@ import { AuthorCard } from "@/components/parts/authorCard";
 import { createMockTip } from "@/repositories/tips/mock";
 import { ActionIcon, Badge, Card, Flex, Stack, Text, Title } from "@mantine/core";
 import { BookmarkIcon, Link1Icon } from "@radix-ui/react-icons";
-import { TipEditButton } from "../../edit/components/TipEditButton/TipEditButton";
 
 export const TipDetailContent: React.FC = () => {
   const tip = createMockTip();
 
   return (
-    <Flex gap={32}>
+    <Flex gap={32} mt={64}>
       <Card shadow="xs" padding="xl" radius="lg" h="fit-content" w="100%">
         <Stack gap={16}>
           <Stack gap={4}>
@@ -35,7 +34,6 @@ export const TipDetailContent: React.FC = () => {
           </ActionIcon>
         </Flex>
         <AuthorCard user={tip.author} />
-        <TipEditButton id={tip.id} />
       </Stack>
     </Flex>
   );
