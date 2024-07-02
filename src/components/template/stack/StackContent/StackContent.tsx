f7a8cc4ff234aab4e30b04405c334a915912c721
imaimai17468 <tosiki_801_life@outlook.jp>
2024-07-02 23:14:42 +0900

    refactor: Separate StackContent and TopContent components into individual files
    The StackContent and TopContent components in 'src/components/template/stack/StackContent/StackContent.tsx' and 'src/components/template/top/TopCotent/TopContent.tsx' have been refactored to use separate component files for improved code organization and maintainability.

--- src/components/template/stack/StackContent/StackContent.tsx
+++ src/components/template/stack/StackContent/StackContent.tsx
@@ -1,12 +1,19 @@
 import { TipList } from "@/components/parts/TipList";
-import { Stack, Text } from "@mantine/core";
+import { CLIENT_PATHS } from "@/constants/clientPaths";
+import { Button, Flex, Stack, Text } from "@mantine/core";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
 
 export const StackContent: React.FC = () => {
   return (
     <Stack>
-      <Text size="xl" fw="bold">
-        Your Stacks
-      </Text>
+      <Flex justify="space-between">
+        <Text size="xl" fw="bold">
+          Your Stacks
+        </Text>
+        <Button variant="subtle" rightSection={<ArrowRightIcon />} component="a" href={CLIENT_PATHS.STACK}>
+          Your Tips
+        </Button>
+      </Flex>
       <TipList />
     </Stack>
   );
--- src/components/template/top/TopCotent/TopContent.tsx
+++ src/components/template/top/TopCotent/TopContent.tsx
@@ -1,12 +1,24 @@
 import { TipList } from "@/components/parts/TipList";
-import { Stack, Text } from "@mantine/core";
+import { CLIENT_PATHS } from "@/constants/clientPaths";
+import { ActionIcon, Button, Flex, Stack, Text } from "@mantine/core";
+import { ArrowRightIcon, PlusIcon } from "@radix-ui/react-icons";
 
 export const TopContent: React.FC = () => {
   return (
     <Stack>
-      <Text size="xl" fw="bold">
-        Your Tips
-      </Text>
+      <Flex justify="space-between">
+        <Flex align="center" gap={16}>
+          <Text size="xl" fw="bold">
+            Your Tips
+          </Text>
+          <ActionIcon variant="outline" radius="xl" component="a" href={CLIENT_PATHS.TIP_CREATE}>
+            <PlusIcon />
+          </ActionIcon>
+        </Flex>
+        <Button variant="subtle" rightSection={<ArrowRightIcon />} component="a" href={CLIENT_PATHS.STACK}>
+          Stack Tips
+        </Button>
+      </Flex>
       <TipList />
     </Stack>
   );
