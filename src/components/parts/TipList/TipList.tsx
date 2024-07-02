ebf3cc6f5eda03801b57163a46f4bd0b59a2abcb
imaimai17468 <tosiki_801_life@outlook.jp>
2024-07-02 23:06:25 +0900

    refactor: Separate TipList component into individual files
    The TipList component in 'src/components/parts/TipList/TipList.tsx' has been refactored to use separate component files for the TipCard component. This improves code organization and maintainability.

--- src/components/parts/TipList/TipList.tsx
+++ src/components/parts/TipList/TipList.tsx
@@ -1,8 +1,10 @@
+import { createMockTips } from "@/repositories/tips/mock";
 import { SimpleGrid } from "@mantine/core";
-import type { Tip } from "../../../repositories/tips/types";
 import { TipCard } from "./components/TipCard";
 
-export const TipList: React.FC<{ tips: Tip[] }> = ({ tips }) => {
+export const TipList: React.FC = () => {
+  const tips = createMockTips(10);
+
   return (
     <SimpleGrid cols={3} spacing="lg" verticalSpacing="lg">
       {tips.map((tip) => (
