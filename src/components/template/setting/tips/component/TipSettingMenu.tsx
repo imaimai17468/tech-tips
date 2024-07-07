835829ab19fd8b4c96935b9b78cf6792e16ae656
imaimai17468 <tosiki_801_life@outlook.jp>
2024-07-07 17:29:46 +0900

    refactor: Update TipSettingMenu component to use Modal for delete confirmation
    The TipSettingMenu component has been updated to use the Modal component for the delete confirmation dialog. This improves the user experience by providing a more visually appealing and consistent way to confirm the deletion of a tip.

--- src/components/template/setting/tips/component/TipSettingMenu.tsx
+++ src/components/template/setting/tips/component/TipSettingMenu.tsx
@@ -1,22 +1,53 @@
 "use client";
 
-import { ActionIcon, Menu } from "@mantine/core";
-import { DoubleArrowDownIcon, DownloadIcon, TrashIcon } from "@radix-ui/react-icons";
+import { ActionIcon, Alert, Button, Menu, Modal, Stack, Text } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { DoubleArrowDownIcon, DownloadIcon, ExclamationTriangleIcon, TrashIcon } from "@radix-ui/react-icons";
 
 export const TipSettingMenu: React.FC = () => {
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const handleDelete = () => {
+    console.log("Delete tip");
+    close();
+  };
+
   return (
-    <Menu position="bottom-end">
-      <Menu.Target>
-        <ActionIcon radius="xl" size="lg" variant="subtle">
-          <DoubleArrowDownIcon />
-        </ActionIcon>
-      </Menu.Target>
-      <Menu.Dropdown>
-        <Menu.Item leftSection={<DownloadIcon />}>Export in Markdown</Menu.Item>
-        <Menu.Item leftSection={<TrashIcon />} color="red">
-          Delete
-        </Menu.Item>
-      </Menu.Dropdown>
-    </Menu>
+    <>
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Delete Tip"
+        centered
+        overlayProps={{
+          backgroundOpacity: 0.55,
+          blur: 3,
+        }}
+        transitionProps={{ transition: "rotate-left" }}
+      >
+        <Stack>
+          <Text>Are you sure you want to delete this tip?</Text>
+          <Alert color="orange" icon={<ExclamationTriangleIcon />} variant="light">
+            Deleting a tip cannot be undone.
+          </Alert>
+          <Button color="red" onClick={handleDelete} leftSection={<TrashIcon />}>
+            Delete
+          </Button>
+        </Stack>
+      </Modal>
+      <Menu position="bottom-end">
+        <Menu.Target>
+          <ActionIcon radius="xl" size="lg" variant="subtle">
+            <DoubleArrowDownIcon />
+          </ActionIcon>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Item leftSection={<DownloadIcon />}>Export in Markdown</Menu.Item>
+          <Menu.Item leftSection={<TrashIcon />} color="red" onClick={open}>
+            Delete
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+    </>
   );
 };
