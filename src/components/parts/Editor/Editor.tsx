8884c3908d24b58934586ed940620654f1373d64
imaimai17468 <tosiki_801_life@outlook.jp>
2024-07-08 01:16:01 +0900

    refactor: Add Editor style and import it in Editor component

--- src/components/parts/Editor/Editor.tsx
+++ src/components/parts/Editor/Editor.tsx
@@ -3,6 +3,7 @@ import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
 import { useCreateBlockNote } from "@blocknote/react";
+import "./style.css";
 
 type props = {
   onChange?: (value: string) => void;
