249c832ca21ced985af995ea7f44a56f356afd60
imaimai17468 <tosiki_801_life@outlook.jp>
2024-07-10 02:12:48 +0900

    refactor: Update UserButton component to conditionally render based on user authentication status

--- src/components/parts/Header/components/UserButton.tsx
+++ src/components/parts/Header/components/UserButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CLIENT_PATHS } from "@/constants/clientPaths";
+import { useAuth } from "@/context/auth";
 import { logout } from "@/lib/auth";
 import { replaceIDinPath } from "@/utils/replaceIDinPath";
 import { Avatar, Menu, UnstyledButton } from "@mantine/core";
@@ -13,31 +14,35 @@ type Props = {
 };
 
 export const UserButton: React.FC<Props> = ({ href, userName, userID }) => {
+  const user = useAuth();
+
   return (
-    <Menu offset={15} withArrow position="bottom-end" transitionProps={{ transition: "rotate-right", duration: 150 }}>
-      <Menu.Target>
-        <UnstyledButton>
-          <Avatar src={href} alt="profile-icon" style={{ border: "1px solid #cdcdcd" }} />
-        </UnstyledButton>
-      </Menu.Target>
-      <Menu.Dropdown>
-        <Menu.Item component="a" href={replaceIDinPath(CLIENT_PATHS.USER, userID)}>
-          {userName}
-        </Menu.Item>
+    user && (
+      <Menu offset={15} withArrow position="bottom-end" transitionProps={{ transition: "rotate-right", duration: 150 }}>
+        <Menu.Target>
+          <UnstyledButton>
+            <Avatar src={href} alt="profile-icon" style={{ border: "1px solid #cdcdcd" }} />
+          </UnstyledButton>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Item component="a" href={replaceIDinPath(CLIENT_PATHS.USER, userID)}>
+            {userName}
+          </Menu.Item>
 
-        <Menu.Label>Settings</Menu.Label>
-        <Menu.Item leftSection={<PersonIcon />} component="a" href={CLIENT_PATHS.SETTINGS_PROFILE}>
-          Profile
-        </Menu.Item>
-        <Menu.Item leftSection={<Pencil1Icon />} component="a" href={CLIENT_PATHS.SETTINGS_TIPS}>
-          Tips
-        </Menu.Item>
+          <Menu.Label>Settings</Menu.Label>
+          <Menu.Item leftSection={<PersonIcon />} component="a" href={CLIENT_PATHS.SETTINGS_PROFILE}>
+            Profile
+          </Menu.Item>
+          <Menu.Item leftSection={<Pencil1Icon />} component="a" href={CLIENT_PATHS.SETTINGS_TIPS}>
+            Tips
+          </Menu.Item>
 
-        <Menu.Label>Account</Menu.Label>
-        <Menu.Item leftSection={<ExitIcon />} onClick={logout} component="a" href={CLIENT_PATHS.TOP}>
-          Logout
-        </Menu.Item>
-      </Menu.Dropdown>
-    </Menu>
+          <Menu.Label>Account</Menu.Label>
+          <Menu.Item leftSection={<ExitIcon />} onClick={logout} component="a" href={CLIENT_PATHS.TOP}>
+            Logout
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+    )
   );
 };
