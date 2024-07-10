1a32dcd294978b8fd01106a8b6ccd03966e3e96b
imaimai17468 <tosiki_801_life@outlook.jp>
2024-07-10 20:53:39 +0900

    refactor: Update TopContent component to include TechTips introduction and CTA

--- src/components/template/top/TopContent.tsx
+++ src/components/template/top/TopContent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Title, Stack, Button } from "@mantine/core";
+import { Button, Card, Image, Stack, Text, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { AuthModal } from "./component/AuthModal";
 
@@ -8,12 +8,35 @@ export const TopContent: React.FC = () => {
   const [opened, { open, close }] = useDisclosure(false);
 
   return (
-    <Stack align="center">
+    <Stack align="center" gap={48}>
       <Title>Where Technologies Rest.</Title>
       <Button onClick={open} variant="light">
         はじめる
       </Button>
       <AuthModal opened={opened} onClose={close} />
+      <Stack gap={16} maw="600px">
+        <Text>TechTipsは、技術的なメモやアイデアを簡単に書き留めるためのサービスです。</Text>
+        <Text>
+          ここでは、完璧な情報や高度な解説を書く必要はありません。周囲の評価や、反響を気にする必要もありません。
+        </Text>
+        <Text>
+          「自分のために、気軽にメモを残す」「いつか誰かが役立てるかもしれない」
+          <br />
+          そんなリラックスした気持ちで、技術的なメモやコードスニペットを保存するための場所です。
+        </Text>
+        <Text>
+          技術的な発見や考えを自由に記録し、整理するためのツールとして、あなたの成長と創造性をサポートします。
+        </Text>
+      </Stack>
+      <Card bg="gray.1" padding="xl" radius="lg" style={{ border: "1px solid #cdcdcd" }}>
+        <Stack align="center" gap={32}>
+          <Text>あなたの静かな技術の場所を作りましょう。</Text>
+          <Image src="/image/coder.svg" alt="Coder" maw={150} />
+          <Button onClick={open} variant="light">
+            はじめる
+          </Button>
+        </Stack>
+      </Card>
     </Stack>
   );
 };
