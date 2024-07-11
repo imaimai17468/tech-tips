b75781bf05557f02777593376b0d543ab943cd63
imaimai17468 <tosiki_801_life@outlook.jp>
2024-07-11 17:41:43 +0900

    refactor: Add Aurora component to MainLayout for visual enhancement

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/parts/Footer";
 import { Header } from "@/components/parts/Header";
 import { Box } from "@mantine/core";
+import { Aurora } from "../parts/Aurora";
 
 type Props = {
   children: React.ReactNode;
@@ -8,12 +9,15 @@ type Props = {
 
 export const MainLayout: React.FC<Props> = ({ children }: Props) => {
   return (
-    <Box mih="100vh">
-      <Header />
-      <Box maw={1120} mx="auto" p={32}>
-        {children}
+    <>
+      <Aurora />
+      <Box mih="100vh">
+        <Header />
+        <Box maw={1120} mx="auto" p={32}>
+          {children}
+        </Box>
+        <Footer />
       </Box>
-      <Footer />
-    </Box>
+    </>
   );
 };
---
+++ src/components/parts/Aurora/Aurora.tsx
@@ -0,0 +1,37 @@
+import { Box } from "@mantine/core";
+
+export const Aurora: React.FC = () => {
+  return (
+    <>
+      <style>
+        {`
+      @keyframes aurora {
+        0% {
+          background-position: 50% 50%, 50% 50%;
+        }
+        100% {
+          background-position: 350% 50%, 350% 50%;
+        }
+      }
+      `}
+      </style>
+      <Box
+        pos="absolute"
+        top={0}
+        left={0}
+        w="100%"
+        h="500px"
+        style={{
+          backgroundImage:
+            "repeating-linear-gradient(100deg,#fff,#fff 7%,transparent 10%,transparent 12%,#fff 16%),repeating-linear-gradient(100deg,#60a5fa 10%,#e879f9 16%,#5eead4 22%,#60a5fa 30%)",
+          WebkitMaskImage: "radial-gradient(ellipse at 100% 0,#fff 10%,transparent 70%)",
+          zIndex: -1,
+          backgroundPosition: "50% 50%,50% 50%",
+          backgroundSize: "300%,200%",
+          animation: "aurora 180s ease-in-out -70s infinite alternate",
+          opacity: 0.3,
+        }}
+      />
+    </>
+  );
+};
