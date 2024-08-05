257a150ad478991ae85d0209f4a742847abfc3b5
imaimai17468 <tosiki_801_life@outlook.jp>
2024-08-05 22:52:20 +0900

    feat: Topページでのログイン部分を実装

--- src/components/template/top/component/StartButton.tsx
+++ src/components/template/top/component/StartButton.tsx
@@ -1,25 +1,23 @@
 "use client";
 
 import { CLIENT_PATHS } from "@/constants/clientPaths";
+import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import { Button } from "@mantine/core";
 import { useRouter } from "next/navigation";
 
-type Props = {
-  isLogin: boolean;
-  isLoading: boolean;
-  open: () => void;
-};
-
-export const StartButton: React.FC<Props> = ({ isLogin, isLoading, open }) => {
+export const StartButton: React.FC = () => {
   const router = useRouter();
 
-  return isLogin ? (
-    <Button variant="light" loading={isLoading} onClick={() => router.push(CLIENT_PATHS.TIP)}>
-      自分のページへ
-    </Button>
-  ) : (
-    <Button onClick={open} variant="light">
-      はじめる
-    </Button>
+  return (
+    <>
+      <SignedIn>
+        <Button variant="light" onClick={() => router.push(CLIENT_PATHS.TIP)}>
+          自分のページへ
+        </Button>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </>
   );
 };
