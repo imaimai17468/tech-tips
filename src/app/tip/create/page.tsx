5ea5137b3d89ef0b5ae32683536a4410459b2952
imaimai17468 <tosiki_801_life@outlook.jp>
2024-08-08 00:13:26 +0900

    feat: createページのmetaデータを設定

--- src/app/tip/create/page.tsx
+++ src/app/tip/create/page.tsx
@@ -1,4 +1,13 @@
 import { TipCreateContent } from "@/components/template/tip/create/TipCreateContent";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create Tip | TechTips",
+  description: "あなたの技術Tipsを作成しましょう",
+  robots: {
+    index: true,
+  },
+};
 
 export default function Home() {
   return <TipCreateContent />;
