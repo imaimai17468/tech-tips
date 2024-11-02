6ecf89d171a88403843a9efba448a95888d984c2
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-02 17:56:46 +0900

    feat: tip追加ボタンを文字で示す

--- src/components/template/tip/TipContent.tsx
+++ src/components/template/tip/TipContent.tsx
@@ -1,8 +1,8 @@
 import { TipList } from "@/components/parts/TipList";
 import { CLIENT_PATHS } from "@/constants/clientPaths";
 import { createMockTips } from "@/repositories/tips/mock";
-import { ActionIcon, Button, Flex, Stack, Title } from "@mantine/core";
-import { ArrowRightIcon, PlusIcon } from "@radix-ui/react-icons";
+import { Button, Flex, Stack, Title } from "@mantine/core";
+import { ArrowRightIcon, PlusCircledIcon } from "@radix-ui/react-icons";
 
 export const TipContent: React.FC = () => {
   const tips = createMockTips(10);
@@ -12,9 +12,15 @@ export const TipContent: React.FC = () => {
       <Flex justify="space-between" align="end">
         <Flex align="center" gap={16}>
           <Title>Your Tips</Title>
-          <ActionIcon variant="light" radius="xl" component="a" href={CLIENT_PATHS.TIP_CREATE}>
-            <PlusIcon />
-          </ActionIcon>
+          <Button
+            variant="light"
+            radius="xl"
+            component="a"
+            href={CLIENT_PATHS.TIP_CREATE}
+            leftSection={<PlusCircledIcon />}
+          >
+            Create Tip
+          </Button>
         </Flex>
         <Button variant="subtle" rightSection={<ArrowRightIcon />} component="a" href={CLIENT_PATHS.STOCK}>
           Stocked Tips
