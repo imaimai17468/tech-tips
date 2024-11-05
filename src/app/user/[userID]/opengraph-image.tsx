2d70bebaaba36a556b4a3409f82d134e36dc828f
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-06 00:01:41 +0900

    fix: userページのogpの出力方式をjpegに修正

--- src/app/user/[userID]/opengraph-image.tsx
+++ src/app/user/[userID]/opengraph-image.tsx
@@ -9,7 +9,7 @@ export const size = {
   height: 420,
 };
 
-export const contentType = "image/png";
+export const contentType = "image/jpeg";
 
 export default async function Image() {
   const user = createMockUser();
