59e13d2df610a04ef780ccec4afea34d3c1f0b0c
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-07 02:05:32 +0900

    fix: apiのファイル名のtypo修正

--- src/app/api/user/create/route.ts
+++ src/app/api/user/create/route.ts
@@ -6,10 +6,10 @@ import { Webhook } from "svix";
 const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error("Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local");
+    throw new Error("Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local");
   }
 
   const headerPayload = headers();
