42d30a275f790a8cb109af3220be8a30e26f5495
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-07 21:37:07 +0900

    fix: viewportの設定を修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,7 @@ import "@mantine/core/styles.css";
 import type { Viewport } from "next";
 
 export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
+  themeColor: "dark",
 };
 
 const theme = createTheme({
