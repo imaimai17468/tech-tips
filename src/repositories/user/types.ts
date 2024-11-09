e18930fc6f7f34bda5af16a2a4cda7c497cb39ee
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-10 00:57:37 +0900

    fix: twitter, githubのidの最大長を設定

--- src/repositories/user/types.ts
+++ src/repositories/user/types.ts
@@ -5,8 +5,8 @@ export const UserValidator = z
   .object({
     username: z.string().min(1).max(30),
     bio: z.string().max(200).optional(),
-    twitterUsername: z.string().optional(),
-    githubUsername: z.string().optional(),
+    twitterUsername: z.string().max(20).optional(),
+    githubUsername: z.string().max(40).optional(),
     userImageURL: z.string().url().optional(),
   })
   .merge(BaseTypeValidator);
