681ba01b20e9752a90fa25372d971f9f8da45a31
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-10 01:30:07 +0900

    feat: snsカードをconformに対応+値を保存できるように

--- src/components/template/setting/profile/component/SNSCard.tsx
+++ src/components/template/setting/profile/component/SNSCard.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { CLIENT_PATHS } from "@/constants/clientPaths";
+import { updateUserSNS } from "@/repositories/user/actions";
 import type { User } from "@/repositories/user/types";
 import { UserValidator } from "@/repositories/user/types";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
 import { Box, Button, Card, Flex, Image, Stack, Text, TextInput } from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { zodResolver } from "mantine-form-zod-resolver";
-import { useState } from "react";
+import { notifications } from "@mantine/notifications";
+import { redirect } from "next/navigation";
+import { useActionState, useEffect, useState } from "react";
 
 type Props = {
   user: User;
@@ -13,61 +17,82 @@ type Props = {
 
 export const SNSCard: React.FC<Props> = ({ user }) => {
   const [isEdit, setIsEdit] = useState(false);
-  const [githubUsername, setGithubUsername] = useState(user.githubUsername);
-  const [twitterUsername, setTwitterUsername] = useState(user.twitterUsername);
-
-  const form = useForm<User>({
-    mode: "uncontrolled",
-    initialValues: user,
-    validate: zodResolver(UserValidator),
+  const [lastResult, action, isPending] = useActionState(updateUserSNS, undefined);
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      const result = parseWithZod(formData, {
+        schema: UserValidator.pick({ twitterUsername: true, githubUsername: true }),
+      });
+      return result;
+    },
+    defaultValue: user,
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
   });
 
-  const handleFormSubmit = (values: User) => {
-    console.log(values);
-    setIsEdit(false);
-    setGithubUsername(values.githubUsername);
-    setTwitterUsername(values.twitterUsername);
-  };
+  useEffect(() => {
+    if (lastResult?.status === "success") {
+      setIsEdit(false);
+      notifications.show({
+        color: "green",
+        title: "Success",
+        message: "SNS information updated",
+      });
+      redirect(CLIENT_PATHS.SETTINGS_PROFILE);
+    } else if (lastResult?.status === "error") {
+      notifications.show({
+        color: "red",
+        title: "Error",
+        message: "Failed to update SNS information",
+      });
+    }
+  }, [lastResult]);
 
   return (
     <Card shadow="xs" padding="xl" radius="lg">
       <Stack>
         <Text size="lg">SNS</Text>
         {isEdit ? (
-          <form onSubmit={form.onSubmit(handleFormSubmit)}>
+          <form id={form.id} onSubmit={form.onSubmit} action={action} noValidate>
             <Stack>
               <Flex align="center" gap={16}>
-                <Image src="/image/github-mark.svg" w={24} h={24} />
+                <Box bg="white" p={2} style={{ borderRadius: "50%" }}>
+                  <Image src="/image/github-mark.svg" w={24} h={24} />
+                </Box>
                 <TextInput
                   leftSection={"@"}
                   placeholder="your github username"
-                  key={form.key("githubUsername")}
-                  {...form.getInputProps("githubUsername")}
+                  key={fields.githubUsername.key}
+                  name={fields.githubUsername.name}
+                  defaultValue={fields.githubUsername.initialValue}
+                  error={fields.githubUsername.errors?.join(", ")}
                 />
               </Flex>
               <Flex align="center" gap={16}>
-                <Box bg="black" p={6} style={{ borderRadius: "50%" }}>
+                <Box bg="black" p={8} style={{ borderRadius: "50%" }}>
                   <Image src="/image/logo.svg" w={12} h={12} />
                 </Box>
                 <TextInput
                   leftSection={"@"}
                   placeholder="your twitter username"
-                  key={form.key("twitterUsername")}
-                  {...form.getInputProps("twitterUsername")}
+                  key={fields.twitterUsername.key}
+                  name={fields.twitterUsername.name}
+                  defaultValue={fields.twitterUsername.initialValue}
+                  error={fields.twitterUsername.errors?.join(", ")}
                 />
               </Flex>
               <Flex gap={16}>
                 <Button
                   onClick={() => {
                     setIsEdit(false);
-                    form.reset();
                   }}
                   variant="subtle"
                   color="red"
                 >
                   Cancel
                 </Button>
-                <Button variant="outline" w="fit-content" type="submit">
+                <Button variant="outline" w="fit-content" type="submit" loading={isPending}>
                   Save SNS User Name
                 </Button>
               </Flex>
@@ -76,14 +101,16 @@ export const SNSCard: React.FC<Props> = ({ user }) => {
         ) : (
           <Stack>
             <Flex align="center" gap={16}>
-              <Image src="/image/github-mark.svg" w={24} h={24} />
-              <Text c="gray">@{githubUsername}</Text>
+              <Box bg="white" p={2} style={{ borderRadius: "50%" }} w="fit-content">
+                <Image src="/image/github-mark.svg" w={24} h={24} />
+              </Box>
+              <Text c="gray">@{user.githubUsername}</Text>
             </Flex>
             <Flex align="center" gap={16}>
-              <Box bg="black" p={6} style={{ borderRadius: "50%" }}>
+              <Box bg="black" p={8} style={{ borderRadius: "50%" }} w="fit-content">
                 <Image src="/image/logo.svg" w={12} h={12} />
               </Box>
-              <Text c="gray">@{twitterUsername}</Text>
+              <Text c="gray">@{user.twitterUsername}</Text>
             </Flex>
             <Button variant="light" w="fit-content" onClick={() => setIsEdit(true)}>
               Edit SNS User Name
