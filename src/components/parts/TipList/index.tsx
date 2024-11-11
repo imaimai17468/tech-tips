3fd4f5387f869a2470fe07d3454612431f76f222
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-12 02:53:18 +0900

    feat: tipList側でfetchするように修正

--- src/components/parts/TipList/index.tsx
+++ src/components/parts/TipList/index.tsx
@@ -1,12 +1,39 @@
-import type { Tip } from "@/repositories/tips/types";
+import { getTipsByAuthorID, getTipsByLoggedInUser } from "@/repositories/tips/actions";
 import { SimpleGrid } from "@mantine/core";
+import { Alert } from "@mantine/core";
+import { ExclamationTriangleIcon, InfoCircledIcon } from "@radix-ui/react-icons";
 import { TipCard } from "./components/TipCard";
 
 type Props = {
-  tips: Tip[];
+  type:
+    | {
+        user: "logged-in";
+      }
+    | {
+        user: "public";
+        authorId: string;
+      };
 };
 
-export const TipList: React.FC<Props> = ({ tips }) => {
+export const TipList: React.FC<Props> = async ({ type }) => {
+  const tips = type.user === "logged-in" ? await getTipsByLoggedInUser() : await getTipsByAuthorID(type.authorId);
+
+  if (!tips) {
+    return (
+      <Alert color="red" icon={<ExclamationTriangleIcon />} title="Failed to retrieve tips">
+        Failed to retrieve tips.
+      </Alert>
+    );
+  }
+
+  if (tips.length === 0) {
+    return (
+      <Alert color="gray" icon={<InfoCircledIcon />} title="No tips found">
+        Try writing some tips!
+      </Alert>
+    );
+  }
+
   return (
     <SimpleGrid cols={{ base: 1, sm: 2, md: 3 }} spacing="lg" verticalSpacing="lg">
       {tips.map((tip) => (
