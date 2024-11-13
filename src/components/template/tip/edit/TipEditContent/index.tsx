3adba0d2c0e7d775e714547b32da6b2498cc6bc7
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-14 02:15:11 +0900

    fix: tip作成にmock userデータは不要なので削除

--- src/components/template/tip/edit/TipEditContent/index.tsx
+++ src/components/template/tip/edit/TipEditContent/index.tsx
@@ -1,15 +1,13 @@
 import { createMockTipForm } from "@/repositories/tips/mock";
-import { createMockUser } from "@/repositories/user/mock";
 import { Box } from "@mantine/core";
 import { TipEditForm } from "./TipEditFrom";
 
 export const TipEditContent: React.FC = () => {
-  const user = createMockUser();
   const initialValues = createMockTipForm();
 
   return (
     <Box>
-      <TipEditForm user={user} initialValues={initialValues} />
+      <TipEditForm initialValues={initialValues} />
     </Box>
   );
 };
