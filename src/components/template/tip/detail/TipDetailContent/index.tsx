f40117a8c361c80365061f25a60ee3dd630d1696
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-14 02:32:07 +0900

    fix: tip詳細ページで実際の値を取得するように修正

--- src/components/template/tip/detail/TipDetailContent/index.tsx
+++ src/components/template/tip/detail/TipDetailContent/index.tsx
@@ -1,53 +1,39 @@
-"use client";
-
 import { AuthorCard } from "@/components/parts/AuthorCard";
 import { CLIENT_PATHS } from "@/constants/clientPaths";
-import { createMockTip } from "@/repositories/tips/mock";
+import { getTipByID } from "@/repositories/tips/actions/get";
 import { replaceIDinPath } from "@/utils/replaceIDinPath";
-import { ActionIcon, Badge, Box, Button, Card, Flex, Stack, Title } from "@mantine/core";
-import { LoadingOverlay } from "@mantine/core";
+import { auth } from "@clerk/nextjs/server";
+import { ActionIcon, Button, Flex, Stack } from "@mantine/core";
 import { BookmarkIcon } from "@radix-ui/react-icons";
 import { Pencil1Icon } from "@radix-ui/react-icons";
-import dynamic from "next/dynamic";
 import Link from "next/link";
 import { ClipButton } from "./ClipButton";
+import { TipArticleContent } from "./TipArticleContent";
 
-const Editor = dynamic(() => import("@/components/parts/Editor").then((v) => v.Editor), {
-  loading: () => <LoadingOverlay visible loaderProps={{ type: "bars" }} />,
-  ssr: false,
-});
+type Props = {
+  tipID: string;
+};
 
-export const TipDetailContent: React.FC = () => {
-  const tip = createMockTip();
+export const TipDetailContent: React.FC<Props> = async ({ tipID }) => {
+  const tip = await getTipByID(tipID);
+  const { userId } = await auth();
 
   return (
     <Flex gap={32} direction={{ base: "column", md: "row" }}>
       <Stack w="100%">
-        <Button
-          ml="auto"
-          variant="light"
-          w="fit-content"
-          component={Link}
-          href={replaceIDinPath(CLIENT_PATHS.TIP_EDIT, tip.id)}
-          rightSection={<Pencil1Icon />}
-        >
-          Edit
-        </Button>
-        <Card shadow="xs" padding="xl" radius="lg" h="fit-content" w="100%">
-          <Stack gap={16}>
-            <Title>{tip.title}</Title>
-            {tip.tags && (
-              <Flex gap={8}>
-                {tip.tags.map((tag) => (
-                  <Badge key={tag}>{tag}</Badge>
-                ))}
-              </Flex>
-            )}
-            <Box mih="50vh">
-              <Editor defaultValue={tip.content} editable={false} />
-            </Box>
-          </Stack>
-        </Card>
+        {userId === tip.author.id && (
+          <Button
+            ml="auto"
+            variant="light"
+            w="fit-content"
+            component={Link}
+            href={replaceIDinPath(CLIENT_PATHS.TIP_EDIT, tip.id)}
+            rightSection={<Pencil1Icon />}
+          >
+            Edit
+          </Button>
+        )}
+        <TipArticleContent tip={tip} />
       </Stack>
       <Stack>
         <Flex gap={8}>
