4d6296104ef82454e867b6da36597695d08196ca
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-14 02:14:51 +0900

    fix: editFormを実際に値を送信できるように修正

--- src/components/template/tip/edit/TipEditContent/TipEditFrom/index.tsx
+++ src/components/template/tip/edit/TipEditContent/TipEditFrom/index.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import { createInitialTipForm } from "@/repositories/tips/mock";
-import { type Tip, type TipForm, TipFormValidator } from "@/repositories/tips/types";
-import type { User } from "@/repositories/user/types";
+import { CLIENT_PATHS } from "@/constants/clientPaths";
+import { createTip } from "@/repositories/tips/actions/create";
+import { updateTip } from "@/repositories/tips/actions/update";
+import { TipValidator } from "@/repositories/tips/types";
+import { useForm, useInputControl } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
 import { Box, Button, Card, Divider, Flex, LoadingOverlay, Stack, Switch, TagsInput, TextInput } from "@mantine/core";
-import { useForm } from "@mantine/form";
+import { notifications } from "@mantine/notifications";
 import { PaperPlaneIcon } from "@radix-ui/react-icons";
-import { zodResolver } from "mantine-form-zod-resolver";
 import dynamic from "next/dynamic";
-import { typeid } from "typeid-js";
+import { useRouter } from "next/navigation";
+import { useActionState, useEffect } from "react";
+import type { Tip } from "../../../../../../repositories/tips/types";
 
 const Editor = dynamic(() => import("@/components/parts/Editor").then((v) => v.Editor), {
   loading: () => <LoadingOverlay visible loaderProps={{ type: "bars" }} />,
@@ -16,64 +20,90 @@ const Editor = dynamic(() => import("@/components/parts/Editor").then((v) => v.E
 });
 
 type Props = {
-  user: User;
-  initialValues?: TipForm;
+  initialValues?: Tip;
 };
 
-export const TipEditForm: React.FC<Props> = ({ user, initialValues }) => {
-  const form = useForm<TipForm>({
-    mode: "uncontrolled",
-    initialValues: initialValues ?? createInitialTipForm(),
-    validate: zodResolver(TipFormValidator),
+export const TipEditForm: React.FC<Props> = ({ initialValues }) => {
+  const router = useRouter();
+  const [lastResult, action, isPending] = useActionState(initialValues ? updateTip : createTip, undefined);
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      const result = parseWithZod(formData, {
+        schema: TipValidator.pick({ title: true, tags: true, content: true, isPublic: true }),
+      });
+      return result;
+    },
+    defaultValue: initialValues ?? {
+      isPublic: false,
+    },
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
   });
+  const contentControl = useInputControl(fields.content);
 
-  const handleFormSubmit = (values: TipForm) => {
-    const submitData: Tip = {
-      ...values,
-      id: typeid().toString(),
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      author: user,
-    };
-
-    console.log(submitData);
-  };
+  useEffect(() => {
+    if (lastResult?.status === "success") {
+      notifications.show({
+        color: "green",
+        title: "Success",
+        message: "Bio updated",
+      });
+      router.push(CLIENT_PATHS.TIP);
+    } else if (lastResult?.status === "error") {
+      notifications.show({
+        color: "red",
+        title: "Error",
+        message: "Failed to update Bio",
+      });
+    }
+  }, [lastResult, router.push]);
 
   return (
     <Box w="100%">
-      <form onSubmit={form.onSubmit(handleFormSubmit)}>
+      <form id={form.id} onSubmit={form.onSubmit} action={action} noValidate>
         <Stack gap={32}>
           <Stack>
             <Flex justify="end" align="center" gap={16}>
               <Switch
                 label="Public"
-                key={form.key("isPublic")}
-                {...form.getInputProps("isPublic", { type: "checkbox" })}
+                key={fields.isPublic.key}
+                name={fields.isPublic.name}
+                defaultValue={fields.isPublic.initialValue}
+                error={fields.isPublic.errors?.join(", ")}
               />
-              <Button variant="light" w="fit-content" type="submit" rightSection={<PaperPlaneIcon />}>
+              <Button
+                variant="light"
+                w="fit-content"
+                type="submit"
+                rightSection={<PaperPlaneIcon />}
+                loading={isPending}
+              >
                 Post
               </Button>
             </Flex>
             <Card shadow="lg" padding="xl" radius="lg" h="fit-content" w="100%">
               <Stack gap={16}>
                 <TextInput
-                  variant="unstyled"
-                  placeholder="Title"
+                  variant="default"
                   label="Title"
-                  key={form.key("title")}
-                  {...form.getInputProps("title")}
+                  key={fields.title.key}
+                  name={fields.title.name}
+                  defaultValue={fields.title.initialValue}
+                  error={fields.title.errors?.join(", ")}
                 />
                 <TagsInput
-                  placeholder="tag1, tag2, ..."
-                  variant="unstyled"
+                  variant="default"
                   label="Tags"
                   description="Press Enter to submit a tag"
-                  key={form.key("tags")}
-                  {...form.getInputProps("tags")}
+                  key={fields.tags.key}
+                  name={fields.tags.name}
+                  defaultValue={fields.tags.initialValue as string[]}
+                  error={fields.tags.errors?.join(", ")}
                 />
                 <Divider />
                 <Box mih="50vh" pl={54}>
-                  <Editor key={form.key("content")} {...form.getInputProps("content")} />
+                  <Editor onChange={contentControl.change} defaultValue={contentControl.value} editable={!isPending} />
                 </Box>
               </Stack>
             </Card>
