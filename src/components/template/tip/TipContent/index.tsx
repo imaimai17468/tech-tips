e7df87e2a405c6b29e98fc522ba8484ed2d20a76
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-15 02:02:36 +0900

    fix: tipページにtip管理ページへのボタンを追加

--- src/components/template/tip/TipContent/index.tsx
+++ src/components/template/tip/TipContent/index.tsx
@@ -1,7 +1,7 @@
 import { TipList } from "@/components/parts/TipList";
 import { CLIENT_PATHS } from "@/constants/clientPaths";
 import { Button, Flex, Skeleton, Stack, Title } from "@mantine/core";
-import { ArrowRightIcon, PlusCircledIcon } from "@radix-ui/react-icons";
+import { ArrowRightIcon, GearIcon, PlusCircledIcon } from "@radix-ui/react-icons";
 import { Suspense } from "react";
 
 export const TipContent: React.FC = async () => {
@@ -18,6 +18,16 @@ export const TipContent: React.FC = async () => {
             leftSection={<PlusCircledIcon />}
           >
             Create Tip
+          </Button>
+          <Button
+            variant="light"
+            radius="xl"
+            component="a"
+            color="gray"
+            href={CLIENT_PATHS.SETTINGS_TIPS}
+            leftSection={<GearIcon />}
+          >
+            Manage Tips
           </Button>
         </Flex>
         <Button variant="subtle" rightSection={<ArrowRightIcon />} component="a" href={CLIENT_PATHS.STOCK}>
