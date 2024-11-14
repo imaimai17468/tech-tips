9242ead646188a6ba98573429b790dfcfd779a49
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-15 00:18:10 +0900

    fix: tipCardの日付とAvatarの位置を修正

--- src/components/parts/TipList/components/TipCard.tsx
+++ src/components/parts/TipList/components/TipCard.tsx
@@ -10,11 +10,22 @@ export const TipCard: React.FC<{ tip: Tip }> = ({ tip }) => {
   return (
     <Card radius="md" padding="md" shadow="sm">
       <Stack gap={16} justify="space-between" h="100%">
-        <Stack gap={4}>
+        <Stack gap={8}>
+          <Flex gap={8} align="center" ml="auto">
+            <Text size="xs">{dayFormat(tip.createdAt)}</Text>
+            {tip.isPublic ? (
+              <Badge variant="light" color="green">
+                Public
+              </Badge>
+            ) : (
+              <Badge variant="light" color="red">
+                Private
+              </Badge>
+            )}
+          </Flex>
           <Anchor href={replaceIDinPath(CLIENT_PATHS.TIP_DETAIL, tip.id)} lineClamp={2} size="lg" fw={700} c="black">
             {tip.title}
           </Anchor>
-          <Text size="xs">{dayFormat(tip.createdAt)}</Text>
           {tip.tags && tip.tags.length > 0 && (
             <Flex gap={8} wrap="wrap">
               {tip.tags.map((tag) => (
@@ -25,11 +36,9 @@ export const TipCard: React.FC<{ tip: Tip }> = ({ tip }) => {
             </Flex>
           )}
         </Stack>
-        <Flex justify="space-between" align="end">
-          <Flex align="center" gap={8}>
-            <Avatar src={tip.author.userImageURL} alt={tip.author.username} />
-            <Text lineClamp={1}>{tip.author.username}</Text>
-          </Flex>
+        <Flex align="center" gap={8} justify="flex-end">
+          <Avatar size="sm" src={tip.author.userImageURL} alt={tip.author.username} />
+          <Text lineClamp={1}>{tip.author.username}</Text>
         </Flex>
       </Stack>
     </Card>
