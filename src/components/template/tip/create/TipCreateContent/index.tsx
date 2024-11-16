c0a2e459ace17c1328a3dc5be54dbb5762827ed2
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-16 22:32:32 +0900

    fix: 使われてないpropsの使用を削除

--- src/components/template/tip/create/TipCreateContent/index.tsx
+++ src/components/template/tip/create/TipCreateContent/index.tsx
@@ -1,13 +1,5 @@
-import { createMockUser } from "@/repositories/user/mock";
-import { Box } from "@mantine/core";
 import { TipEditForm } from "../../edit/TipEditContent/TipEditFrom";
 
 export const TipCreateContent: React.FC = () => {
-  const user = createMockUser();
-
-  return (
-    <Box>
-      <TipEditForm user={user} />
-    </Box>
-  );
+  return <TipEditForm />;
 };
