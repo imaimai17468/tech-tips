13193f0812d28e9e7c8207fef287a4ba21639025
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-17 20:39:25 +0900

    fix: pathからexploreを削除

--- src/constants/clientPaths.ts
+++ src/constants/clientPaths.ts
@@ -1,7 +1,6 @@
 export const CLIENT_PATHS = {
   TOP: "/",
   STOCK: "/stock",
-  EXPLORE: "/explore",
   TIP: "/tip",
   TIP_DETAIL: "/tip/[id]",
   TIP_CREATE: "/tip/create",
@@ -20,7 +19,6 @@ const createRegexPaths = (paths: readonly string[]) => {
 
 export const AUTH_REQUIRED_PATHS_REGEX = createRegexPaths([
   CLIENT_PATHS.STOCK,
-  CLIENT_PATHS.EXPLORE,
   CLIENT_PATHS.TIP,
   CLIENT_PATHS.TIP_CREATE,
   CLIENT_PATHS.TIP_EDIT,
