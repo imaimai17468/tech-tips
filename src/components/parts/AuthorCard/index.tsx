9b638d8ef1abcfb3e4e6f867aa8f1e961b182f97
imaimai17468 <tosiki_801_life@outlook.jp>
2024-11-17 20:46:10 +0900

    fix: AuthorCardのユーザーネームでユーザーページへ遷移するように修正

--- src/components/parts/AuthorCard/index.tsx
+++ src/components/parts/AuthorCard/index.tsx
@@ -1,4 +1,6 @@
+import { CLIENT_PATHS } from "@/constants/clientPaths";
 import type { User } from "@/repositories/user/types";
+import { replaceIDinPath } from "@/utils/replaceIDinPath";
 import { Anchor, Avatar, Card, Flex, Stack, Text } from "@mantine/core";
 import { SNSButtons } from "../SNSButtons";
 
@@ -12,7 +14,7 @@ export const AuthorCard: React.FC<Props> = ({ user }) => {
       <Stack gap={16}>
         <Flex align="center" gap={8}>
           <Avatar src={user.userImageURL} alt={user.username} />
-          <Anchor lineClamp={1} c="black" fw={700}>
+          <Anchor lineClamp={1} c="black" fw={700} href={replaceIDinPath(CLIENT_PATHS.USER, user.id)}>
             {user.username}
           </Anchor>
         </Flex>
