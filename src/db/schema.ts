4428756547937a6830061df11e963f60268de64a
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-15 21:43:53 +0900

    feat: implement row-level security policies for users and stocks tables to enforce authenticated role access

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,7 @@
 import { sql } from "drizzle-orm";
 import { relations } from "drizzle-orm";
-import { boolean, pgTable, primaryKey, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { boolean, pgPolicy, pgTable, primaryKey, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { authenticatedRole } from "drizzle-orm/supabase";
 
 export const users = pgTable("users", {
   id: text("id").primaryKey(),
@@ -63,3 +64,46 @@ export const stocksRelations = relations(stocks, ({ one }) => ({
     references: [tips.id],
   }),
 }));
+
+export const stocksPolicy = pgPolicy("authenticated role all policy", {
+  for: "all",
+  to: authenticatedRole,
+  using: sql`requesting_user_id() = clerk_user_id`,
+  withCheck: sql`requesting_user_id() = clerk_user_id`,
+}).link(stocks);
+
+export const usersInsertPolicy = pgPolicy("authenticated role insert policy", {
+  for: "insert",
+  to: authenticatedRole,
+  withCheck: sql`requesting_user_id() = clerk_user_id`,
+}).link(users);
+
+export const usersUpdatePolicy = pgPolicy("authenticated role update policy", {
+  for: "update",
+  to: authenticatedRole,
+  withCheck: sql`requesting_user_id() = clerk_user_id`,
+}).link(users);
+
+export const usersDeletePolicy = pgPolicy("authenticated role delete policy", {
+  for: "delete",
+  to: authenticatedRole,
+  using: sql`requesting_user_id() = clerk_user_id`,
+}).link(users);
+
+export const tipsInsertPolicy = pgPolicy("authenticated role insert policy", {
+  for: "insert",
+  to: authenticatedRole,
+  withCheck: sql`requesting_user_id() = clerk_user_id`,
+}).link(tips);
+
+export const tipsUpdatePolicy = pgPolicy("authenticated role update policy", {
+  for: "update",
+  to: authenticatedRole,
+  withCheck: sql`requesting_user_id() = clerk_user_id`,
+}).link(tips);
+
+export const tipsDeletePolicy = pgPolicy("authenticated role delete policy", {
+  for: "delete",
+  to: authenticatedRole,
+  using: sql`requesting_user_id() = clerk_user_id`,
+}).link(tips);
