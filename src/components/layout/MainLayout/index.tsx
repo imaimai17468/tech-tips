a953f2709218fd7d1be1ff27622b9ea473f34b66
imaimai17468 <tosiki_801_life@outlook.jp>
2025-01-17 23:58:26 +0900

    fix: correct import path for Aurora component in MainLayout; ensure consistent component structure and improve code organization

--- src/components/layout/MainLayout/index.tsx
+++ src/components/layout/MainLayout/index.tsx
@@ -1,7 +1,7 @@
+import { Aurora } from "@/components/parts/Aurora";
 import { Footer } from "@/components/parts/Footer";
 import { Header } from "@/components/parts/Header";
 import { Box } from "@mantine/core";
-import { Aurora } from "../parts/Aurora";
 
 type Props = {
   children: React.ReactNode;
