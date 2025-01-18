fe801fd1739a5ccff10b2b4808ee2cd1eaba12c9
imaimai17468 <tosiki_801_life@outlook.jp>
2025-01-19 00:43:59 +0900

    feat: refactor TipContent component layout; improve responsiveness and structure by adjusting Flex properties and button visibility for enhanced user experience

--- src/components/template/tip/TipContent/index.tsx
+++ src/components/template/tip/TipContent/index.tsx
@@ -8,29 +8,49 @@ export const TipContent: React.FC = async () => {
   return (
     <Stack gap={32}>
       <Flex justify="space-between" align="end">
-        <Flex align="center" gap={16}>
-          <Title>Your Tips</Title>
-          <Button
-            variant="light"
-            radius="xl"
-            component="a"
-            href={CLIENT_PATHS.TIP_CREATE}
-            leftSection={<PlusCircledIcon />}
-          >
-            Create Tip
-          </Button>
-          <Button
-            variant="light"
-            radius="xl"
-            component="a"
-            color="gray"
-            href={CLIENT_PATHS.SETTINGS_TIPS}
-            leftSection={<GearIcon />}
-          >
-            Manage Tips
-          </Button>
+        <Flex w="100%" align={{ base: "flex-start", xs: "center" }} gap={16} direction={{ base: "column", xs: "row" }}>
+          <Flex justify="space-between" align="center" gap={16} w={{ base: "100%", xs: "fit-content" }}>
+            <Title>Your Tips</Title>
+            <Button
+              variant="subtle"
+              rightSection={<ArrowRightIcon />}
+              component="a"
+              href={CLIENT_PATHS.STOCK}
+              hiddenFrom="xs"
+            >
+              Stocked Tips
+            </Button>
+          </Flex>
+          <Flex gap={16}>
+            <Button
+              variant="light"
+              radius="xl"
+              component="a"
+              href={CLIENT_PATHS.TIP_CREATE}
+              leftSection={<PlusCircledIcon />}
+            >
+              Create Tip
+            </Button>
+            <Button
+              variant="light"
+              radius="xl"
+              component="a"
+              color="gray"
+              href={CLIENT_PATHS.SETTINGS_TIPS}
+              leftSection={<GearIcon />}
+            >
+              Manage Tips
+            </Button>
+          </Flex>
         </Flex>
-        <Button variant="subtle" rightSection={<ArrowRightIcon />} component="a" href={CLIENT_PATHS.STOCK}>
+        <Button
+          variant="subtle"
+          rightSection={<ArrowRightIcon />}
+          component="a"
+          href={CLIENT_PATHS.STOCK}
+          visibleFrom="xs"
+          style={{ flexShrink: 0 }}
+        >
           Stocked Tips
         </Button>
       </Flex>
