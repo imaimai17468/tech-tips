a3e2b666719ff16884bf5d816b5a3f9628dab984
imaimai17468 <tosiki_801_life@outlook.jp>
2025-01-19 01:00:26 +0900

    feat: refactor UserProfileCard component; enhance layout and responsiveness by replacing Image with Avatar, adjusting padding and flex properties for improved user experience

--- src/components/template/user/UserContent/UserProfileCard/index.tsx
+++ src/components/template/user/UserContent/UserProfileCard/index.tsx
@@ -1,7 +1,6 @@
 import { SNSButtons } from "@/components/parts/SNSButtons";
 import { getUserByID } from "@/repositories/user/actions/get";
-import { Card, Flex, Stack, Text } from "@mantine/core";
-import Image from "next/image";
+import { Avatar, Card, Flex, Stack, Text } from "@mantine/core";
 
 type Props = {
   userID: string;
@@ -11,17 +10,21 @@ export const UserProfileCard: React.FC<Props> = async ({ userID }) => {
   const user = await getUserByID(userID);
 
   return (
-    <Card radius="md" shadow="sm" p={32}>
-      <Flex gap={32} wrap={{ base: "wrap", sm: "nowrap" }}>
-        <Image
-          width={100}
-          height={100}
-          src={user.userImageURL || ""}
-          alt={user.username}
-          style={{ borderRadius: "50%" }}
-        />
+    <Card radius="md" shadow="sm" p={{ base: 16, xs: 32 }}>
+      <Flex gap={{ base: 16, xs: 32 }} direction={{ base: "column", xs: "row" }}>
+        <Flex gap={16} align="center">
+          <Avatar
+            src={user.userImageURL || ""}
+            alt={user.username}
+            w={{ base: 42, xs: 100 }}
+            h={{ base: 42, xs: 100 }}
+          />
+          <Text size="xl" fw="bold" hiddenFrom="xs">
+            {user.username}
+          </Text>
+        </Flex>
         <Stack>
-          <Text size="xl" fw="bold">
+          <Text size="xl" fw="bold" visibleFrom="xs">
             {user.username}
           </Text>
           <Text style={{ whiteSpace: "pre-line" }}>{user.bio}</Text>
